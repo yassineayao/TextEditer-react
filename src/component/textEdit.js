c319d71198b6a7555cd97ff425a68b81fbd34e3b
youssef tadlaoui <usftadlaoui@gmail.com>
2023-05-17 00:21:37 +0100

    EditText

--- src/component/textEdit.js
+++ src/component/textEdit.js
@@ -1,3 +1,16 @@
 import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+
+
+function TextEditor() {
+  const [text, setText] = useState('');
+
+  const handleTextChange = (value) => {
+    setText(value);
+  };
+
+  return (
+    <ReactQuill value={text} onChange={handleTextChange} />
+  );
+}
